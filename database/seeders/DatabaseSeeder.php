21b1c82bc49a6f6b38994e2277ca780a0c8bd523
Degsus01 <pachy_@outlook.com>
2025-12-01 04:22:01 -0500

    actualiz

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
 {
     $this->call([
         RolesTableSeeder::class,
-        AdminUserSeeder::class,
-        PagosSeeder::class, // opcional
+        AdminUserSeeder::class, // opcional
     ]);
 }
 
