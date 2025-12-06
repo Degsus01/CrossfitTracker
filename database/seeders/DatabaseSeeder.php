bacbaf5e554bf06155999ed6ac9dfb2f16762ffe
Degsus01 <pachy_@outlook.com>
2025-12-06 18:40:09 -0500

    nayellis

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
 {
     $this->call([
         RolesTableSeeder::class,
-        AdminUserSeeder::class, // opcional
+        AdminUserSeeder::class,
+        EntrenadoresSeeder::class, // opcional
     ]);
 }
 
